17eab43dc4ec976853a429f207cc1c65c28978e1
AlanRacciatti <alanracciatti1220@gmail.com>
2022-01-17 15:00:24 -0300

    update collection link

--- frontend/src/components/ConnectedContainer.jsx
+++ frontend/src/components/ConnectedContainer.jsx
@@ -3,7 +3,7 @@ const ConnectedContainer = (props) => (
       <button onClick={props.function} ref={props.reference} className="cta-button connect-wallet-button">
         Mint NFT
       </button>
-      <a href='https://testnets.opensea.io/collection/lolnft-hxyvhrmy5h' target='_blank' rel='noreferrer'><button className="cta-button connect-wallet-button mint-button">
+      <a href='https://testnets.opensea.io/collection/lolnft-esi3q5cx0e' target='_blank' rel='noreferrer'><button className="cta-button connect-wallet-button mint-button">
         Explore collection
       </button></a>
     </div>
